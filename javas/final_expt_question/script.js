70fe456344a4dbbb96413dd74b870b1397ecba86
Daminipalrecha <sonudimpi78@gmail.com>
2017-03-11 15:24:01 +0530

    html's script

---
+++ javas/final_expt_question/script.js
@@ -0,0 +1,27 @@
+// DOM manipulation
+console.log(document.getElementById("title"));
+console.log(document instanceof HTMLDocument);
+
+
+function div() {
+	var no1 = document.getElementById("no1").value;
+	var no2 = document.getElementById("no2").value;
+
+	if (no1 > no2) {
+		var first = no1 ;
+		var second = no2 ;
+	}
+	if (no2 > no1) {
+		var first = no2 ;
+		var second = no1 ;
+	}
+
+	var rem = first/second ;
+	document.getElementById("result").innerHTML = "Remainder :"+rem ;
+
+	var display = document.querySelector("#title").textContent ;
+	display = "And Remainder is : "+rem+" !!!!" ;
+	document.querySelector("h1").textContent = display ;
+}
+
+
