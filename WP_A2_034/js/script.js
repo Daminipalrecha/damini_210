bb4ad31e6698f857b649ecd9a59df8ac7f8b1480
Daminipalrecha <sonudimpi78@gmail.com>
2017-04-12 00:49:35 +0530

    A2 m2 wp

--- WP_A2_034/js/script.js
+++ WP_A2_034/js/script.js
@@ -5,44 +5,50 @@ document.addEventListener("DOMContentLoaded",
     // Unobtrusive event binding
     document.querySelector("button")
       .addEventListener("click", function () {
-        
-        // Call server to get the name
-        $ajaxUtils
-          .sendGetRequest("data/name.json", 
-            function (res) {
+                 
+              if ( temp === res.student1) {
+                 // Call server to get the name
+              $ajaxUtils
+              .sendGetRequest("data/name.json", 
+              function (res) {
               var temp = document.getElementById("name").value;
               var ans = "" ;
               var msg = "" ;
-                 
-              if ( temp === res.student1) {
-                msg = res.ques1+"<br>" ;
-                ans = document.querySelector("input[name=btn]:checked").value;
-                if (res.ans == res.answer1) {
-                  msg+="Nice, right answer.<br>" ;
-                }
-                else{
+              msg = res.ques1+"<br>" ;
+              ans = document.querySelector("input[name=btn]:checked").value;
+              if (res.ans == res.answer1) {
+                 msg+="Nice, right answer.<br>" ;
+               }
+               else{
                   msg+="Right answer is True.<br>" ;
                 }
-              }
+              });
+            }
                 
               else if (temp === res.student2) {
-                msg = res.ques2+"<br>" ;
-                ans = document.querySelector("input[name=btn]:checked").value;
-                if (res.ans == res.answer2) {
+                 // Call server to get the name
+              $ajaxUtils
+              .sendGetRequest("data/name.json", 
+              function (res) {
+              var temp = document.getElementById("name").value;
+              var ans = "" ;
+              var msg = "" ;
+              msg = res.ques2+"<br>" ;
+              ans = document.querySelector("input[name=btn]:checked").value;
+              if (res.ans == res.answer2) {
                   msg+="Nice, right answer.<br>" ;
-                }
-                else{
+               }
+               else{
                   msg+="Right answer is False.<br>" ;
                 }
-              }
-
-              else {
+              
+              });
+          }
+            else {
                 msg = "no such student";
               }
 
-
               document.getElementById("content").innerHtml = msg ;
             });
-      });
+    });
   
-  });
