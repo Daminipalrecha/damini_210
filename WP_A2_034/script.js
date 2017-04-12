069993b04bdc9ac197b89c1b4b6f5dc29f62f1bf
Daminipalrecha <sonudimpi78@gmail.com>
2017-04-12 12:07:47 +0530

    m3bwp

--- WP_A2_034/script.js
+++ WP_A2_034/script.js
@@ -13,18 +13,23 @@ document.addEventListener("DOMContentLoaded",
               function (res) {
             
               var msg = "No such student !!" ;
-              var count=0;
+              var count=0; //first position is 0
               // var sapn = res.sap ;
               // var names = res.name ; 
               for (var i=0 ; i<10 ; i++){
+
                 if(temp == res.sap[i]){
-                  msg = "Name is :"+res.name[i] +" and position is : "+count;
+
+                  msg = "Name is :"+ res.name[i] +" and position is : "+count;
+                  // console.log(res.sap[i]);
+                  // console.log(msg);
                   count=count +1 ;
                   break ;
 
                 }
 
               }
+
                document.getElementById("content").innerHTML = msg ;
 
             });
